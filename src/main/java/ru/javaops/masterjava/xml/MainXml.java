36cade8abdea89e309ed4d97bff6f765992c69c3
Крупин Евгений <eekrupin@svyaznoy.ru>
2017-11-03 08:48:10 +0300

    HW02 prep MainXml

---
+++ src/main/java/ru/javaops/masterjava/xml/MainXml.java
@@ -0,0 +1,34 @@
+package ru.javaops.masterjava.xml;
+
+import com.google.common.io.Resources;
+import ru.javaops.masterjava.xml.schema.ObjectFactory;
+import ru.javaops.masterjava.xml.schema.Payload;
+import ru.javaops.masterjava.xml.util.JaxbParser;
+import ru.javaops.masterjava.xml.util.Schemas;
+
+import java.io.IOException;
+
+/**
+ * Created by eekrupin on 03.11.2017.
+ */
+public class MainXml {
+
+    private static final JaxbParser JAXB_PARSER = new JaxbParser(ObjectFactory.class);
+
+    static {
+        JAXB_PARSER.setSchema(Schemas.ofClasspath("payload.xsd"));
+    }
+
+    public static void main(String[] args) throws Exception {
+        printUsersByProject("TopJava");
+    }
+
+    public static void printUsersByProject(String project) throws Exception {
+        Payload payload = JAXB_PARSER.unmarshal(
+                Resources.getResource("payload.xml").openStream());
+        String strPayload = JAXB_PARSER.marshal(payload);
+        JAXB_PARSER.validate(strPayload);
+        System.out.println(strPayload);
+    }
+
+}

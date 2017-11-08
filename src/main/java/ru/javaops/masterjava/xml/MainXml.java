25070f3bc855da4d8626d0a9613d07278596f7f7
Крупин Евгений <eekrupin@svyaznoy.ru>
2017-11-08 16:11:05 +0300

    HW02 after review

--- src/main/java/ru/javaops/masterjava/xml/MainXml.java
+++ src/main/java/ru/javaops/masterjava/xml/MainXml.java
@@ -1,10 +1,20 @@
 package ru.javaops.masterjava.xml;
 
+import com.google.common.base.Splitter;
 import com.google.common.io.Resources;
 import ru.javaops.masterjava.xml.schema.*;
 import ru.javaops.masterjava.xml.util.JaxbParser;
 import ru.javaops.masterjava.xml.util.Schemas;
+import ru.javaops.masterjava.xml.util.StaxStreamProcessor;
+
+import javax.xml.stream.XMLStreamReader;
+import javax.xml.stream.events.XMLEvent;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.*;
 import java.util.function.Consumer;
+
+import static com.google.common.base.Strings.nullToEmpty;
 
 
 /**
@@ -13,27 +23,67 @@ import java.util.function.Consumer;
 public class MainXml {
 
     private static final JaxbParser JAXB_PARSER = new JaxbParser(ObjectFactory.class);
+    private static final Comparator<User> USER_COMPARABLE = Comparator.comparing(User::getFullName).thenComparing(User::getEmail);
 
     static {
         JAXB_PARSER.setSchema(Schemas.ofClasspath("payload.xsd"));
     }
 
     public static void main(String[] args) throws Exception {
-//        printUsersByProject("TopJava");
-//        printUsersByProject("MasterJava");
-        createHTMLUsersByProject("TopJava");
+        printUsersByProject("TopJava");
+        //printUsersByProject("MasterJava");
+        //createHTMLUsersByProject("TopJava");
     }
 
     public static void printUsersByProject(String project) throws Exception {
         Consumer<Object> printer = System.out::println;
-        Payload payload = JAXB_PARSER.unmarshal(
-                Resources.getResource("payload.xml").openStream());
+        URL payloadUrl = Resources.getResource("payload.xml");
 
-        payload.getUsers().getUser().stream()
-                .filter(user -> user.getGroup().stream()
-                        .filter(group -> project.equals( ( (ProjectType) ((GroupType) group).getProject() ).getName() )).count() > 0
-                )
-                .forEach( user -> printer.accept(String.format("%s/%s", user.getFullName(), user.getEmail())) );
+        try (InputStream is = payloadUrl.openStream()){
+            StaxStreamProcessor processor = new StaxStreamProcessor(is);
+            final Set<String> groupNames = new HashSet<>();
+
+            while (processor.startElement("Group", "Groups")){
+                String groupName = processor.getAttribute("name");
+                if (project.equals(processor.getElementValue("project"))){
+                    groupNames.add(groupName);
+                 }
+            }
+
+            if (groupNames.isEmpty()){
+                throw new IllegalArgumentException("Invalid "+ project + "or no groups");
+
+            }
+
+            Set<User> users = new TreeSet<>(USER_COMPARABLE);
+
+            JaxbParser parser = new JaxbParser(User.class);
+            while (processor.doUntil(XMLEvent.START_ELEMENT, "User")){
+
+                User user = new User();
+                user.setFlag(FlagType.valueOf(processor.getAttribute("flag").toUpperCase()));
+                user.setEmail(processor.getAttribute("email"));
+                user.setFullName(processor.getElementValue("fullName"));
+
+                String groupRefs = processor.getElementValue("group");
+                if (!Collections.disjoint(groupNames, Splitter.on(' ').splitToList(nullToEmpty(groupRefs)))){
+                    users.add(user);
+                }
+            }
+
+            users.forEach( user -> printer.accept(String.format("%s/%s", user.getFullName(), user.getEmail())) );
+
+        }
+
+
+//        Payload payload = JAXB_PARSER.unmarshal(
+//                Resources.getResource("payload.xml").openStream());
+//
+//        payload.getUsers().getUser().stream()
+//                .filter(user -> user.getGroup().stream()
+//                        .filter(group -> project.equals( ( (ProjectType) ((GroupType) group).getProject() ).getName() )).count() > 0
+//                )
+//                .forEach( user -> printer.accept(String.format("%s/%s", user.getFullName(), user.getEmail())) );
 
 //        String strPayload = JAXB_PARSER.marshal(payload);
 //        JAXB_PARSER.validate(strPayload);
--- src/main/java/ru/javaops/masterjava/xml/util/StaxStreamProcessor.java
+++ src/main/java/ru/javaops/masterjava/xml/util/StaxStreamProcessor.java
@@ -53,4 +53,30 @@ public class StaxStreamProcessor implements AutoCloseable {
             }
         }
     }
+
+    public boolean startElement(String element, String parent) throws XMLStreamException {
+        while (reader.hasNext()) {
+            int event = reader.next();
+            if (parent != null && isElementEnd(event, parent)) {
+                return false;
+            }
+            if (isElementStart(event, element)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isElementStart(int event, String el) {
+        return event == XMLEvent.START_ELEMENT && el.equals(reader.getLocalName());
+    }
+
+    private boolean isElementEnd(int event, String el) {
+        return event == XMLEvent.END_ELEMENT && el.equals(reader.getLocalName());
+    }
+
+    public String getAttribute(String name) throws XMLStreamException {
+        return reader.getAttributeValue(null, name);
+    }
+
 }
